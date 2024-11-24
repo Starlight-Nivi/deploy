128104a1f8de1b92571b40ff931cbcb677c5ad40
Starlight-Nivi <159890768+Starlight-Nivi@users.noreply.github.com>
2024-11-25 00:25:24 +0530

    create os.py

--- os.py
+++ os.py
@@ -1,17 +1,9 @@
-name: Super-Linter
+def hello():
+    print("hi")
 
-on: push
 
-jobs:
-  super-lint:
-    name: Lint code base
-    runs-on: ubuntu-latest
-    steps:
-      - name: Checkout code
-        uses: actions/checkout@v2
+def bye():
+    print("bye")
 
-      - name: Run Super-Linter
-        uses: github/super-linter@v4
-        env:
-          DEFAULT_BRANCH: main
-          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+
+print(hello())
